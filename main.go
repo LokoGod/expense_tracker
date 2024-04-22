62050575b79de4a3fe3cce1cb20fbe722f47bdeb
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-04-22 21:34:55 +0530

    init

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
-import "fmt"
-import "github.com/gin-gonic/gin"
+// import "fmt"
+import (
+
+	"github.com/LokoGod/expense_tracker/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
 
 func main() {
+
 	router := gin.Default()
-	router.GET("/ping", func(ctx *gin.Context) {
+
+	router.GET("/api/v1/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
