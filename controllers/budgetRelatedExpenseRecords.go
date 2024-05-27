a43eab4ace5a69789e0407b84afa74154f374a83
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-05-27 15:44:55 +0530

    production commit 02

--- controllers/budgetRelatedExpenseRecords.go
+++ controllers/budgetRelatedExpenseRecords.go
@@ -1,17 +1,23 @@
 package controllers
 
-//func FetchAllBudgetRelatedExpenseRecords(c *gin.Context) {
-//
-//	id := c.Param("RelatedBudgetID")
-//
-//	var relatedExpenseRecords []models.ExpenseRelatedBudget
-//	initializers.DB.Where("Related_Budget_ID = ?", id).Find(&relatedExpenseRecords)
-//
-//	c.JSON(200, gin.H{
-//		"All related records": relatedExpenseRecords,
-//	})
-//}
-//
+import (
+	"github.com/LokoGod/expense_tracker/initializers"
+	"github.com/LokoGod/expense_tracker/models"
+	"github.com/gin-gonic/gin"
+)
+
+func FetchAllBudgetRelatedExpenseRecords(c *gin.Context) {
+
+	id := c.Param("BudgetID")
+
+	var relatedExpenseRecords []models.Expense
+	initializers.DB.Where("Budget_ID = ?", id).Find(&relatedExpenseRecords)
+
+	c.JSON(200, gin.H{
+		"All related records": relatedExpenseRecords,
+	})
+}
+
 //func CalTotalBudgetRelatedExpenseRecordAmount(c *gin.Context) {
 //	id := c.Param("RelatedBudgetID")
 //
--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 	router.POST("/api/v1/budget", controllers.AddBudget)
 	router.DELETE("/api/v1/budget/:id", controllers.DeleteBudget)
 
-	//router.GET("/api/v1/relatedExpenseRecord/:RelatedBudgetID", controllers.FetchAllBudgetRelatedExpenseRecords)
+	router.GET("/api/v1/relatedExpenseRecord/:BudgetID", controllers.FetchAllBudgetRelatedExpenseRecords)
 	//router.GET("/api/v1/totalRelatedRecordAmount/:RelatedBudgetID", controllers.CalTotalBudgetRelatedExpenseRecordAmount)
 	//router.GET("/api/v1/calBudgetRemaining/:RelatedBudgetID", controllers.CalBudgetRemaining)
 
